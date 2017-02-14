b18a1365e61c9488fbd564f843250ea2864d463b
Faiqoh <faiqoh11.fa@gmail.com>
2017-02-14 11:08:40 +0700

    setor done

--- src/setor.java
+++ src/setor.java
@@ -13,16 +13,16 @@ import javax.swing.JOptionPane;
  */
 public class setor {
     private int saldo;
-    public void menabung(int bal, int s) {
-        if (s%100!=0)
+    public void menabung(int tab, int se) {
+        if (se%100!=0)
             JOptionPane.showMessageDialog(null,
                     "Mesin ATM BRI tidak menerima uang koin \nSilakan ulani kembali!",
                     "Transaksi Gagal",0);
-        else if(s<50000)
+        else if(se<50000)
             JOptionPane.showMessageDialog(null,
                     "Besaran minimal setor tunai adalah Rp 50.000,00","ERROR",0);
-        else if(s>50000){
-            saldo = bal+s;
+        else if(se>50000){
+            saldo = tab+se;
             JOptionPane.showMessageDialog(null,
                     "Saldo Anda saat ini sebesar : "+saldo);
         }
